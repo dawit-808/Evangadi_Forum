8767d34f9836a7832cfb3031333f409f892def1a
Dawit <tesfayedawit22090582@gmail.com>
2025-06-29 15:13:45 +0300

    axios bug fix

--- client/src/Axios.jsx
+++ client/src/Axios.jsx
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_BASE_URL
-  ? `${import.meta.env.VITE_BASE_URL}/api`
-  : "http://localhost:5500/api";
-
 const axiosInstance = axios.create({
-  baseURL: base,
+  baseURL: "https://evangadi-forum-rkc2.onrender.com/api",
 });
 
 export default axiosInstance;
