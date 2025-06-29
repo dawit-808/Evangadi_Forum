e0729dcc12fd77821ca78fd0748f812884e7de63
Dawit <tesfayedawit22090582@gmail.com>
2025-06-29 15:09:19 +0300

    axios update for production

--- client/src/Axios.jsx
+++ client/src/Axios.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const base = import.meta.env.VITE_BASE_URL
+  ? `${import.meta.env.VITE_BASE_URL}/api`
+  : "http://localhost:5500/api";
+
 const axiosInstance = axios.create({
-  baseURL:
-    `${import.meta.env.VITE_BASE_URL}/api` || "http://localhost:5500/api",
+  baseURL: base,
 });
 
 export default axiosInstance;
